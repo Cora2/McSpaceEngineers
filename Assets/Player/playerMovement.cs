b041d4d7bd993f0b6cd078add95369ca6293ded1
Cora2 <44071549+Cora2@users.noreply.github.com>
2021-10-12 12:40:00 -0600

    - Added Player Movement

---
+++ Assets/Player/playerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float walkSpeed = 10f;
+    public float jetpackSpeed = 75;
+    public bool jetpack = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("j") && jetpack == false)
+        {
+            Debug.Log("Jetpack On");
+            jetpack = true;
+        }
+        else if (Input.GetKeyDown("j"))
+        {
+            Debug.Log("Jetpack Off");
+            jetpack = false;
+        }
+        if (jetpack == false)
+        {
+            this.GetComponent<Rigidbody2D>().gravityScale = 1;
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += move * walkSpeed * Time.deltaTime;
+        }
+        else {
+            this.GetComponent<Rigidbody2D>().gravityScale = 0;
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += move * walkSpeed * Time.deltaTime;
+        }
+    }
+}
