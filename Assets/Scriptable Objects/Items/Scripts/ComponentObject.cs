1161b5e1f5863470a9a27c211e9360c7637d3682
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2021-10-26 14:56:26 -0600

    Update ComponentObject.cs

--- Assets/Scriptable Objects/Items/Scripts/ComponentObject.cs
+++ Assets/Scriptable Objects/Items/Scripts/ComponentObject.cs
@@ -1,1 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+
+public class ComponentObject : ItemObject
+{
+    public int Integrity;
+    public int BaseProductionTime; // seconds
+    public void Awake()
+    {
+        type = ItemType.Component;
+    }
+}
